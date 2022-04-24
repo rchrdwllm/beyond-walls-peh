3975a42bf50923f3f4ea99eac3fd1e56baaf2ca5
Richard William <rwilliam.flores@gmail.com>
2022-04-24 10:00:48 +0800

    added resize observer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,13 @@ import type { AppProps } from "next/app";
 import "styles/globals.scss";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 
+import { install } from "resize-observer";
+
 import Layout from "components/Layout";
+
+if (typeof window !== "undefined") {
+    install();
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
